161572bb6f1789ea7d7f0080b61f5f2c7e829ecd
KAikiGma <91917989+KAikiGma@users.noreply.github.com>
2023-11-23 16:23:20 -0300

    Add files via upload

--- php-beginner-crud-level-1/config/database.php
+++ php-beginner-crud-level-1/config/database.php
@@ -1,14 +1,16 @@
 <?php
-
+// used to connect to the database
+// usado para conectar no banco de dados
 $host = "localhost";
-$db_name = "php_beginner_crud_level_1";
-$username "root";
+$db_name = "php-beginner-crud-level-1";
+$username = "root";
 $password = "";
 try {
-    $con = new PDO("mysql:host=($host);$dbname={$db_name}", $username, $password);
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
 }
-
-catch(PDOException $exception) {
+// show error
+// mostrar erros
+catch(PDOException $exception){
     echo "Connection error: " . $exception->getMessage();
 }
 ?>
